f6948a3e3f9f4077bdc244532b1d884a2140a94b
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-06-29 19:05:12 +0300

    Polish

--- library/HasqlDev.hs
+++ library/HasqlDev.hs
@@ -34,6 +34,7 @@ module HasqlDev
     -- * Transactions
     Transaction.Transaction,
     Transaction.Sessions.IsolationLevel (..),
+    Transaction.Sessions.Mode (..),
 
     -- ** Transaction execution
     RunsTransaction (..),
@@ -64,7 +65,7 @@ import HasqlDev.Prelude
 
 -- |
 -- Capability of a functor to execute sessions.
-class (Functor f) => RunsSession f where
+class (Monad f) => RunsSession f where
   -- | Lift a session into the context of the functor.
   runSession :: Session.Session a -> f a
 
@@ -90,7 +91,7 @@ instance (RunsSession f, Monoid w) => RunsSession (WriterT w f) where
   runSession session = WriterT (fmap (\a -> (a, mempty)) (runSession session))
 
 -- | Capability of a functor to execute pipelines.
-class (Functor f) => RunsPipeline f where
+class (Applicative f) => RunsPipeline f where
   -- | Lift a pipeline into the context of the functor.
   runPipeline :: Pipeline.Pipeline a -> f a
 
@@ -100,47 +101,70 @@ instance RunsPipeline Pipeline.Pipeline where
 instance RunsPipeline Session.Session where
   runPipeline = Session.pipeline
 
-instance (RunsPipeline f) => RunsPipeline (StateT s f) where
-  runPipeline session = StateT \s -> fmap (\a -> (a, s)) (runPipeline session)
+instance (Monad f, RunsPipeline f) => RunsPipeline (StateT s f) where
+  runPipeline pipeline = StateT \s -> fmap (\a -> (a, s)) (runPipeline pipeline)
 
-instance (RunsPipeline f) => RunsPipeline (ReaderT r f) where
-  runPipeline session = ReaderT \_ -> runPipeline session
+instance (Monad f, RunsPipeline f) => RunsPipeline (ReaderT r f) where
+  runPipeline pipeline = ReaderT \_ -> runPipeline pipeline
 
-instance (RunsPipeline f) => RunsPipeline (ExceptT e f) where
-  runPipeline session = ExceptT (fmap Right (runPipeline session))
+instance (Monad f, RunsPipeline f) => RunsPipeline (ExceptT e f) where
+  runPipeline pipeline = ExceptT (fmap Right (runPipeline pipeline))
 
-instance (RunsPipeline f, Monoid w) => RunsPipeline (WriterT w f) where
-  runPipeline session = WriterT (fmap (\a -> (a, mempty)) (runPipeline session))
+instance (Monad f, RunsPipeline f, Monoid w) => RunsPipeline (WriterT w f) where
+  runPipeline pipeline = WriterT (fmap (\a -> (a, mempty)) (runPipeline pipeline))
 
-class (Functor f) => RunsTransaction f where
+class (Monad f) => RunsTransaction f where
   -- | Lift a transaction into the context of the functor.
   runTransaction ::
-    Transaction.Transaction a ->
-    -- | Whether the transaction writes.
-    Bool ->
     -- | Transaction isolation level.
     Transaction.Sessions.IsolationLevel ->
+    -- | Transaction mode.
+    Transaction.Sessions.Mode ->
+    Transaction.Transaction a ->
     f a
 
 instance RunsTransaction Session.Session where
-  runTransaction transaction isWrite isolationLevel =
-    Transaction.Sessions.transaction isolationLevel mode transaction
-    where
-      mode =
-        if isWrite
-          then Transaction.Sessions.Write
-          else Transaction.Sessions.Read
+  runTransaction = Transaction.Sessions.transaction
+
+instance (RunsTransaction f) => RunsTransaction (StateT s f) where
+  runTransaction isolationLevel mode transaction =
+    StateT \s -> fmap (\a -> (a, s)) (runTransaction isolationLevel mode transaction)
+
+instance (RunsTransaction f) => RunsTransaction (ReaderT r f) where
+  runTransaction isolationLevel mode transaction =
+    ReaderT \_ -> runTransaction isolationLevel mode transaction
+
+instance (RunsTransaction f) => RunsTransaction (ExceptT e f) where
+  runTransaction isolationLevel mode transaction =
+    ExceptT (fmap Right (runTransaction isolationLevel mode transaction))
+
+instance (RunsTransaction f, Monoid w) => RunsTransaction (WriterT w f) where
+  runTransaction isolationLevel mode transaction =
+    WriterT (fmap (\a -> (a, mempty)) (runTransaction isolationLevel mode transaction))
 
 -- | Capability of a functor to execute unparameterized and possibly multistatement SQL-queries.
-class (Functor f) => RunsScript f where
+class (Monad f) => RunsScript f where
+  -- | Execute an unparameterized and possibly multistatement SQL script in the context of the functor.
   runScript :: Text -> f ()
 
 instance RunsScript Session.Session where
   runScript = Session.script
 
+instance (RunsScript f) => RunsScript (StateT s f) where
+  runScript sql = StateT \s -> fmap (\a -> (a, s)) (runScript sql)
+
+instance (RunsScript f) => RunsScript (ReaderT r f) where
+  runScript sql = ReaderT \_ -> runScript sql
+
+instance (RunsScript f) => RunsScript (ExceptT e f) where
+  runScript sql = ExceptT (fmap Right (runScript sql))
+
+instance (RunsScript f, Monoid w) => RunsScript (WriterT w f) where
+  runScript sql = WriterT (fmap (\a -> (a, mempty)) (runScript sql))
+
 -- |
 -- Capability of a functor to execute statements.
-class (Functor f) => RunsStatement f where
+class (Applicative f) => RunsStatement f where
   -- | Execute a statement in the context of the functor, providing the parameters for it.
   runStatement :: Statement.Statement a b -> a -> f b
 
@@ -152,6 +176,18 @@ instance RunsStatement Session.Session where
 
 instance RunsStatement Transaction.Transaction where
   runStatement = flip Transaction.statement
+
+instance (Monad f, RunsStatement f) => RunsStatement (StateT s f) where
+  runStatement statement params = StateT \s -> fmap (\a -> (a, s)) (runStatement statement params)
+
+instance (Monad f, RunsStatement f) => RunsStatement (ReaderT r f) where
+  runStatement statement params = ReaderT \_ -> runStatement statement params
+
+instance (Monad f, RunsStatement f) => RunsStatement (ExceptT e f) where
+  runStatement statement params = ExceptT (fmap Right (runStatement statement params))
+
+instance (Monad f, RunsStatement f, Monoid w) => RunsStatement (WriterT w f) where
+  runStatement statement params = WriterT (fmap (\a -> (a, mempty)) (runStatement statement params))
 
 -- |
 -- Execute a statement implicitly determined by its parameters in a functor that is capable of running statements.
